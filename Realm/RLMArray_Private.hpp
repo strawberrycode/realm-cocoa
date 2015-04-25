d8c3c1e38ebb33defd02673583329ee2bca518f8
Thomas Goyne <tg@realm.io>
2015-04-25 09:22:16 -0800

    Fix observing List<> properties on standalone objects

--- Realm/RLMArray_Private.hpp
+++ Realm/RLMArray_Private.hpp
@@ -42,12 +42,8 @@ namespace realm {
   @public
     // The name of the property which this RLMArray represents
     NSString *_key;
+    __weak RLMObjectBase *_parentObject;
 }
-
-// initializer
-- (instancetype)initWithObjectClassName:(NSString *)objectClassName
-                           parentObject:(RLMObjectBase *)object
-                                    key:(NSString *)key;
 @end
 
 
