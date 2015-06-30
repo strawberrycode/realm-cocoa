67ad76fcbedc334a1d74e5dc637c8e144fa24054
Samuel E. Giddins <segiddins@segiddins.me>
2015-06-29 17:09:19 -0700

    Add basic nullable NSDate support

--- Realm/RLMProperty.mm
+++ Realm/RLMProperty.mm
@@ -30,6 +30,7 @@ BOOL RLMPropertyTypeIsNullable(RLMPropertyType propertyType) {
 #ifdef REALM_ENABLE_NULL
         case RLMPropertyTypeString:
         case RLMPropertyTypeData:
+        case RLMPropertyTypeDate:
 #endif
             return YES;
         default:
@@ -142,6 +143,9 @@ BOOL RLMPropertyTypeIsNullable(RLMPropertyType propertyType) {
             }
             else if (strcmp(code, "@\"NSDate\"") == 0) {
                 _type = RLMPropertyTypeDate;
+#if REALM_ENABLE_NULL
+                _optional = YES;
+#endif
             }
             else if (strcmp(code, "@\"NSData\"") == 0) {
                 _type = RLMPropertyTypeData;
